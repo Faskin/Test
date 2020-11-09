91f9b4978e002578244a688da088cc6443b48d92
Faskin <alex2002tyn@gmail.com>
2020-11-09 14:19:47 +0200

    Learning

--- Test/ContentView.swift
+++ Test/ContentView.swift
@@ -7,15 +7,28 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+var tutors: [Tutor] = []
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ContentView: View {
+    var tutors: [Tutor] = []
+ 
+    var body: some View {
+        List(tutors) { tutor in
+            Image(tutor.imageName)
+            VStack(alignment: .leading) {
+                Text(tutor.name)
+                Text(tutor.headline)
+                    .font(.subheadline)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            }
+        }
     }
 }
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        //2
+        ContentView(tutors: testData)
+    }
+}
+#endif
